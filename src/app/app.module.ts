c3e10b4d2802207994d48ed81c73c05360181727
gulusuraj <gulusurajverma@gmail.com>
2022-05-15 16:26:08 +0530

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginSignUpComponent } from './login-sign-up/login-sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignUpComponent
   ],
   imports: [
     BrowserModule,
